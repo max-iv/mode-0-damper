7f74707b5bbb0b534f90ee50ddd51a0ebc45dd2f
David McGinnis <david.mcginnis@thequantumguys.se>
2019-03-05 08:22:51 +0100

    as is

---
+++ init-blinky-lite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo su - pi -c "/usr/bin/screen -S mo -d -m /home/pi/blinky-lite/run-mode-0-damper.sh"
+
---
+++ run-mode-0-damper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 5
+cd /home/pi/blinky-lite/mode-0-damper
+source runNodeRed.sh
+
+
